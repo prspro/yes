5c2463c37f9a0ad80418a145ffaa24b06c0a3b76
prosp3ro <prospero1135@gmail.com>
2018-12-05 19:04:10 +0200

    +slick, +reviews(started)

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,1 +1,12 @@
 //Some JS
+$(document).ready(function(){
+    $('.carousel').slick({
+        infinity: true,
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 3,
+        slidesToScroll: 1       
+    });
+});
